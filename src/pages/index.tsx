dd14b423de98becdab45199c63a02f0365c9db86
Prajwal <73160418+prajwxlk@users.noreply.github.com>
2023-02-08 03:35:42 +0000

    basic app is ready

---
+++ src/pages/index.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+const Home = () => {
+  const [text, setText] = useState('');
+  const [data, setData] = useState([]);
+  const [successMessage, setSuccessMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setData([...data, text]);
+    setText('');
+    setSuccessMessage(`"${text}" added successfully`);
+  };
+
+  const handleDownload = () => {
+    const json = JSON.stringify(data);
+    const blob = new Blob([json], { type: 'application/json' });
+    const href = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = href;
+    link.download = 'dataset.json';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Prompt Dataset Generator</h1>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          style={{ width: '500px', height: '200px' }}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {successMessage && <p>{successMessage}</p>}
+      <button onClick={handleDownload} style={{ marginTop: '20px' }}>
+        Generate Dataset
+      </button>
+    </div>
+  );
+};
+
+export default Home;
